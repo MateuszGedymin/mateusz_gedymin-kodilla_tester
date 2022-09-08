c1e80d3a79f62d05dfdd8ebbdc488ba0489f8919
Mateusz Gedymin <mateuszgedymin@gmail.com>
2022-09-08 14:12:17 +0200

    Zadanie: deklarowanie i wywoływanie zmiennych

---
+++ kodilla-intro/src/test/java/MainExtended.java
@@ -0,0 +1,11 @@
+public class MainExtended {
+    public static void main( String[] args) {
+        String first = "first";
+        int second = 2;
+        double threeAndSome = 3.14;
+        boolean fourth = true;
+        String fifth = "fifth";
+        String sixth = "sixth";
+        System.out.println(first + second + threeAndSome + fourth + fifth +  sixth);
+    }
+}
